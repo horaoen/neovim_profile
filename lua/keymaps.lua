642fd412b26db7b879b850fb9b19e59e0a55b503
horaoen <horaoen@gmail.com>
2022-04-24 15:39:27 +0800

    refactor(keymaps.lua): change keymaps for nvimtree

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -80,28 +80,43 @@ local pluginKeys = {}
 -- nvim-tree
 pluginKeys.nvimtreeList = {
 	{ key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
-	-- 分屏打开文件
-	{ key = "v", action = "vsplit" },
-	-- 显示隐藏文件
-	{ key = "h", action = "split" },
-	-- Ignore (node_modules)
-	{ key = "i", action = "toggle_ignored" },
-	-- Hide (dotfiles)
-	{ key = ".", action = "toggle_dotfiles" },
-	-- 文件操作
-	{ key = "<F5>", action = "refresh" },
+	{ key = "<C-e>", action = "edit_in_place" },
+	{ key = { "O" }, action = "edit_no_picker" },
+	{ key = { "<2-RightMouse>", "<C-]>" }, action = "cd" },
+	{ key = "<C-v>", action = "vsplit" },
+	{ key = "<C-x>", action = "split" },
+	{ key = "<C-t>", action = "tabnew" },
+	{ key = "<", action = "prev_sibling" },
+	{ key = ">", action = "next_sibling" },
+	{ key = "P", action = "parent_node" },
+	{ key = "<BS>", action = "close_node" },
+	{ key = "<Tab>", action = "preview" },
+	{ key = "K", action = "first_sibling" },
+	{ key = "J", action = "last_sibling" },
+	{ key = "I", action = "toggle_git_ignored" },
+	{ key = "H", action = "toggle_dotfiles" },
+	{ key = "R", action = "refresh" },
 	{ key = "a", action = "create" },
 	{ key = "d", action = "remove" },
+	{ key = "D", action = "trash" },
 	{ key = "r", action = "rename" },
+	{ key = "<C-r>", action = "full_rename" },
 	{ key = "x", action = "cut" },
 	{ key = "c", action = "copy" },
 	{ key = "p", action = "paste" },
-	-- 进入下一级
-	{ key = { "]" }, action = "cd" },
-	-- 进入上一级
-	{ key = { "[" }, action = "dir_up" },
-	-- mac
-	{ key = "s", action = "open" },
+	{ key = "y", action = "copy_name" },
+	{ key = "Y", action = "copy_path" },
+	{ key = "gy", action = "copy_absolute_path" },
+	{ key = "[c", action = "prev_git_item" },
+	{ key = "]c", action = "next_git_item" },
+	{ key = "-", action = "dir_up" },
+	{ key = "s", action = "system_open" },
+	{ key = "q", action = "close" },
+	{ key = "g?", action = "toggle_help" },
+	{ key = "W", action = "collapse_all" },
+	{ key = "S", action = "search_node" },
+	{ key = "<C-k>", action = "toggle_file_info" },
+	{ key = ".", action = "run_file_command" },
 }
 
 pluginKeys.SymbolList = {
