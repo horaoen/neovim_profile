14c65b93328ba72d8188775facc018fac6572839
horaoen <horaoen@gmail.com>
2022-04-26 08:43:06 +0800

    feat: add dap for go

--- lua/dap-config/nvim-dap.lua
+++ lua/dap-config/nvim-dap.lua
@@ -3,49 +3,7 @@
 local dap = require("dap")
 -- 设置断点样式
 vim.fn.sign_define("DapBreakpoint", { text = "⊚", texthl = "TodoFgFIX", linehl = "", numhl = "" })
-dap.adapters.cppdbg = {
-	id = "cppdbg",
-	type = "executable",
-	command = "/home/horaoen/Dev/cpptools-linux/extension/debugAdapters/bin/OpenDebugAD7",
-}
-dap.configurations.cpp = {
-	{
-		name = "Launch file",
-		type = "cppdbg",
-		request = "launch",
-		program = function()
-			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-		end,
-		cwd = "${workspaceFolder}",
-		stopOnEntry = true,
 
-		setupCommands = {
-			{
-				text = "-enable-pretty-printing",
-				description = "enable pretty printing",
-				ignoreFailures = false,
-			},
-		},
-	},
-	{
-		name = "Attach to gdbserver :1234",
-		type = "cppdbg",
-		request = "launch",
-		MIMode = "gdb",
-		miDebuggerServerAddress = "localhost:1234",
-		miDebuggerPath = "/usr/bin/gdb",
-		cwd = "${workspaceFolder}",
-		program = function()
-			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-		end,
-		setupCommands = {
-			{
-				text = "-enable-pretty-printing",
-				description = "enable pretty printing",
-				ignoreFailures = false,
-			},
-		},
-	},
-}
-dap.configurations.c = dap.configurations.cpp
-dap.configurations.rust = dap.configurations.cpp
+_ = require("dap-config.settings.cppdbg")
+
+_ = require("dap-config.settings.go")
