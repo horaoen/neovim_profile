aa0365d64f1834c8d33464033ab3e9e0bdd3d85f
horaoen <horaoen@gmail.com>
2022-05-11 14:18:41 +0800

    refactor: change client.resolved to client.server

--- lua/lsp-config/settings/rust_analyzer.lua
+++ lua/lsp-config/settings/rust_analyzer.lua
@@ -4,8 +4,8 @@ local opts = {
     debounce_text_changes = 150,
   },
   on_attach = function(client, bufnr)
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
+    client.server_capabilities.document_formatting = false
+    client.server_capabilities.document_range_formatting = false
     local function buf_set_keymap(...)
       vim.api.nvim_buf_set_keymap(bufnr, ...)
     end
