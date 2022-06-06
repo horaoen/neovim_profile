7dc8dd9c6e7ff9c21d1502f7f10a8562a433618e
horaoen <horaoen@gmail.com>
2022-06-06 14:42:00 +0800

    aa

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -11,13 +11,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- 默认的键位设置函数太长了，所以这里将它们重新引用一下
-vim.keybinds = {
-	gmap = vim.api.nvim_set_keymap,
-	bmap = vim.api.nvim_buf_set_keymap,
-	dgmap = vim.api.nvim_del_keymap,
-	dbmap = vim.api.nvim_buf_del_keymap,
-	opts = { noremap = true, silent = true },
-}
 
 -- Edit
 keymap("n", "<leader>q", "<cmd>q<cr>", opts)
