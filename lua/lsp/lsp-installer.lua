7e2af0c16f37f2a9affc6326f5e02bc9221bbc07
root <root@win11.localdomain>
2022-06-09 01:45:52 +0800

    style: modify default shell

--- lua/lsp/lsp-installer.lua
+++ lua/lsp/lsp-installer.lua
@@ -1,39 +1,39 @@
 local path = require("utils.path")
 
 require("nvim-lsp-installer").setup({
-  ensure_installed = require("utils.servers"),
+    ensure_installed = require("utils.servers"),
 
-  automatic_installation = true,
+    automatic_installation = true,
 
-  ui = {
-    check_outdated_servers_on_open = true,
-    icons = {
-      server_installed = "◍",
-      server_pending = "◍",
-      server_uninstalled = "◍",
+    ui = {
+        check_outdated_servers_on_open = true,
+        icons = {
+            server_installed = "◍",
+            server_pending = "◍",
+            server_uninstalled = "◍",
+        },
+        keymaps = {
+            toggle_server_expand = "<CR>",
+            install_server = "i",
+            update_server = "u",
+            check_server_version = "c",
+            update_all_servers = "U",
+            check_outdated_servers = "C",
+            uninstall_server = "X",
+        },
     },
-    keymaps = {
-      toggle_server_expand = "<CR>",
-      install_server = "i",
-      update_server = "u",
-      check_server_version = "c",
-      update_all_servers = "U",
-      check_outdated_servers = "C",
-      uninstall_server = "X",
+
+    install_root_dir = path.join(vim.fn.stdpath("data"), "lsp_servers"),
+
+    pip = {
+        install_args = {},
     },
-  },
 
-  install_root_dir = path.join(vim.fn.stdpath("data"), "lsp_servers"),
+    log_level = vim.log.levels.INFO,
 
-  pip = {
-    install_args = {},
-  },
+    max_concurrent_installers = 4,
 
-  log_level = vim.log.levels.INFO,
-
-  max_concurrent_installers = 4,
-
-  github = {
-    download_url_template = "https://github.com/%s/releases/download/%s/%s",
-  },
+    github = {
+        download_url_template = "https://hub.fastgit.xyz/%s/releases/download/%s/%s",
+    },
 })
